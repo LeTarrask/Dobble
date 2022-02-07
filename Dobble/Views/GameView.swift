a6485f30876112524845c4070c18af27d5932166
leTarrask <tarrask@tarrask.com>
2022-02-07 14:37:25 +0000

    implemented SpriteKit gameview

---
+++ Dobble/Views/GameView.swift
@@ -0,0 +1,45 @@
+//
+//  GameView.swift
+//  Dobble
+//
+//  Created by tarrask on 07/02/2022.
+//
+
+import SwiftUI
+import SpriteKit
+
+struct GameView: View {
+    var scene: SKScene {
+        let scene = GameScene()
+        scene.size = CGSize(width: 300, height: 400)
+        scene.scaleMode = .fill
+        return scene
+    }
+    
+    var body: some View {
+        SpriteView(scene: scene)
+            .frame(width: 300, height: 400)
+            .ignoresSafeArea()
+    }
+}
+
+struct GameView_Previews: PreviewProvider {
+    static var previews: some View {
+        GameView()
+    }
+}
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        let box = SKSpriteNode(color: SKColor.red, size: CGSize(width: 50, height: 50))
+        box.position = location
+        box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 50, height: 50))
+        addChild(box)
+    }
+}
